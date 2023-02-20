daf952232fab97a9834b48898dba4de01b513997
TheWhiteFluid <mitocarustefan@gmail.com>
2023-02-20 04:37:37 +0200

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -34,3 +34,6 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number !';
   }
 });
+
+//---------------------------------------------------------------------
+// IMPLEMENTING THE GAME LOGIC
