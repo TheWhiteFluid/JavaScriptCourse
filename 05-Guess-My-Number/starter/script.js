24e8a75458cfb7dbe3a91504eea6dfaf6ccd4afa
TheWhiteFluid <mitocarustefan@gmail.com>
2023-02-20 04:36:18 +0200

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,5 +23,14 @@ document.querySelector('.guess').value = 23;
 // HANDLING CLICK EVENTS
 
 /* <button class="btn check">Check!</button> */
+/* <input type="number" class="guess" /> */ //type-number --> value
 
-document.querySelector('.check').addEventListener('click', function () {});
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    //guess default value in console is 0 (falsy value)
+    document.querySelector('.message').textContent = 'No number !';
+  }
+});
