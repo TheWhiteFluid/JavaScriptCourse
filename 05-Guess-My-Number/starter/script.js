98b758f1c5dedac5bc8a17bbb88eed31c1a33a7f
TheWhiteFluid <mitocarustefan@gmail.com>
2023-02-21 05:38:04 +0200

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -29,25 +29,46 @@ document.querySelector('.guess').value = 23;
 
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
 
+let score = document.querySelector('.score').textContent;
+
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     //guess default value in console is 0 (falsy value)
     document.querySelector('.message').textContent = 'No number !';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number !';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high !';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high !';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game ! :(';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too low !';
-  } else {
-    document.querySelector('.message').textContent = 'Try again :(';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low !';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game ! :(';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
 
 //---------------------------------------------------------------------
-// IMPLEMENTING THE GAME LOGIC
+// IMPLEMENTING THE GAME LOGIC ( up up )
