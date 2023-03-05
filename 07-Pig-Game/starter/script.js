9f8b4cc0e454a94b8aa71e93363fbf3df73e4722
TheWhiteFluid <mitocarustefan@gmail.com>
2023-03-05 23:57:04 +0200

    sss

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -17,60 +17,88 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-currentScore0El.textContent = 0;
-currentScore1El.textContent = 0;
-
-diceEl.classList.add('hidden');
-
+// defining variables for future use
 let currentScore = 0;
 let activePlayer = 0;
 
-// ROLLING DICE button functionality
-btnRoll.addEventListener('click', function () {
-  // generating random dice roll
-  const diceValue = Math.trunc(Math.random() * 6 + 1);
-  console.log(diceValue);
+let scores = [0, 0];
 
-  // display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${diceValue}.png`;
+let playing = true;
 
-  // check for rolled 1: if true -> switch to next player
-  //               if false -> add score(dice roll) at current score
-  if (diceValue !== 1) {
-    document.getElementById(`score--${activePlayer}`).textContent = diceValue;
-    currentScore = currentScore + diceValue;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
 
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-  }
-});
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
-// HOLD button functionality
-btnHold.addEventListener('click', function () {
-  document.getElementById(`current--${activePlayer}`).textContent =
-    currentScore + diceValue;
-});
-
-// NEW GAME functionality
-btnNew.addEventListener('click', function () {
-  diceEl.classList.add('hidden');
+const init = function () {
   score0El.textContent = 0;
   score1El.textContent = 0;
   currentScore0El.textContent = 0;
   currentScore1El.textContent = 0;
+
   activePlayer = 0;
   currentScore = 0;
+  scores = [0, 0];
+  playing = true;
+
+  diceEl.classList.add('hidden');
 
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+
+//--------- GAME BUTTONS FUNCTIONALITY --------------
+// ROLLING DICE
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // generating random dice roll
+    const diceValue = Math.trunc(Math.random() * 6 + 1);
+    console.log(diceValue);
+
+    // display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceValue}.png`;
+
+    // check for rolled 1: if true -> switch to next player if false -> add score(dice roll) at current score
+    if (diceValue !== 1) {
+      currentScore = currentScore + diceValue;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
 });
+
+// HOLD
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //add player's score to the current score
+    scores[activePlayer] = scores[activePlayer] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //100 points condition - game over
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// NEW GAME
+btnNew.addEventListener('click', init);
