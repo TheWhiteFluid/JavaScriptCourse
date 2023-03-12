aefc572133e3986b8df00e842dd510477d8c3205
TheWhiteFluid <mitocarustefan@gmail.com>
2023-03-12 15:08:45 +0200

    Update script.js

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const firstName = 'Rares';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName} this is your age ${age}, born in year ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      // Creating a NEW variable with same name as outer scope's variable
+      const firstName = 'Marin';
+      const output2 = `Hi ${firstName}, you are a millenial`;
+      console.log(output2);
+
+      var millenial = true;
+
+      // Reasining outer scope's variable
+      output = 'NEW OUTPUT';
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      const sum = add(2, 3);
+      console.log(sum);
+    }
+    console.log(millenial); // work because of var
+    console.log(output); // most recent scope variable declaration(reassining)
+    //console.log(add(2,3)) -> doesn t work because function must be called in the outer scope first
+  }
+
+  printAge();
+  return age;
+}
+
+calcAge(1994);
