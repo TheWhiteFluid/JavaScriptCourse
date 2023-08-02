263c0458d47b950545be22e032c5aa66c06cf078
TheWhiteFluid <mitocarustefan@gmail.com>
2023-08-02 05:06:03 +0300

    Update script.js

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -43,3 +43,74 @@ createBooking('F232', 20, undefined, 100);
 
 // HIGHER ORDER FUNCTIONS
 console.log('HIGHER ORDER FUNCTIONS..........................................');
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLocaleLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [firstWord, ...others] = str.split(' ');
+  return [firstWord.toUpperCase(), ...others].join(' ');
+};
+
+const strTransform = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+};
+
+strTransform('welcome to the JavaScript!', oneWord);
+strTransform('welcome to the JavaScript!', upperFirstWord);
+
+const hi5 = function () {
+  console.log(`Hi! You've clicked me :)`);
+};
+
+document.body.addEventListener('click', hi5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+greet('Hello ')('Steven');
+
+const greetArrow = (greetArrow) => {
+  return (name) => console.log(`${greetArrow} ${name}`);
+};
+
+greetArrow('Hi')('Jonas');
+
+const lufthanse = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push(`flight:${this.iataCode}${flightNum}, ${name}`);
+  },
+};
+console.log(lufthanse);
+lufthanse.book(232, 'Pain');
+
+const euroWings = {
+  airline: 'EuroWings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book2 = function (flightNum, name) {
+  console.log(
+    `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+  );
+  this.bookings.push(`flight:${this.iataCode}${flightNum}, ${name}`);
+};
+
+// doesn't work because this pointer doen't know where to acctually point(object)
+//book2(4226, 'Rava');
+
+// instead we will use call function which indicates where this have to point
+book2.call(euroWings, 339, 'Core');
+book2.call(lufthanse, 4226, 'Ravanneli');
