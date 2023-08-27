390c916e7b2bf097cc960b1b2f2acb5433bb644c
Stefan Mitocaru <mitocarustefan@gmail.com>
2023-08-27 17:53:39 +0300

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -126,23 +126,22 @@ console.log('jonas'.at(-1));
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 console.log('---- FOR OF ----');
-
 //for(const movement of movements) {
 for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`You deposited ${movement} at position ${i + 1}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)} from position ${i + 1}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
 
 console.log('---- FOR EACH ----'); //needs a callback function !!!
-
-movements.forEach(function (movement) {
+//movements.forEach(function (movement) {
+movements.forEach(function (movement, i, arr) {
   if (movement > 0) {
-    console.log(`You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 });
 // 0: function(200)
